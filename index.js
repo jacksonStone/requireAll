9a3a11d36155078c3df0fd1e67b153e38517fa80
Jackson Stone <jackson@skuid.com>
2017-11-19 07:08:14 -0500

    small fix

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-
+const path = require('path');
 module.exports = function(dir, myModule){
 	require("fs").readdirSync(dir).forEach(function(file) {
 		if(file !== "index.js") {
 			var name = file.substring(0, file.length - 3);
-			myModule.exports[name] = require('./'+file);
+			var appDir = path.dirname(require.main.filename);
+			dir = dir.substring(appDir.length);
+			myModule.exports[name] = require(path.resolve('../../', dir, file);
 		}
 	});
 }
