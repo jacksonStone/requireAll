2767b6f2f9b4c4fd463199d8599cac1d923a610a
Jackson Stone <jackson@skuid.com>
2017-11-19 08:13:04 -0500

    Only pull in JS files

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = function(dir, myModule){
 	require("fs").readdirSync(dir).forEach(function(file) {
-		if(file !== "index.js") {
+		if(file !== "index.js" && file.indexOf('.js')!== -1) {
 			var name = file.substring(0, file.length - 3);
 			var appDir = path.dirname(require.main.filename);
 			var dir0 = dir.substring(appDir.length);
