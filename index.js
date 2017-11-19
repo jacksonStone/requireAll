2a0d80a2e8798b52ea09e22a488e58c324439c7d
Jackson Stone <jackson@skuid.com>
2017-11-19 07:15:41 -0500

    small fix

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ module.exports = function(dir, myModule){
 		if(file !== "index.js") {
 			var name = file.substring(0, file.length - 3);
 			var appDir = path.dirname(require.main.filename);
-			dir = dir.substring(appDir.length);
-			myModule.exports[name] = require(path.resolve('../../', dir, file);
+			var dir0 = dir.substring(appDir.length);
+			myModule.exports[name] = require('../..'+ dir0 + '/' + file);
 		}
 	});
 }
